fb13d7d4089483e416a4acf2323bfa3fa453d72b
CLoudXVII <sharkov.vs@outlook.com>
2025-04-10 18:42:34 +0300

    workspace switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { DottedSeparator } from "./dotted-separator";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -10,6 +11,8 @@ export const Sidebar = () => {
       <Link href="/">
         <Image src="/logo.svg" alt="logo" width={48} height={48} />
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
